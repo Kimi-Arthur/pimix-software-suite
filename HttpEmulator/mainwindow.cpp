8186446ff5c28489aeffed01bd7f47c0b48a92ce
Kimi Arthur <Kimi Arthur@localhost>
2012-12-17 22:23:58 +0800

    Function done... Need some more display part.

--- HttpEmulator/mainwindow.cpp
+++ HttpEmulator/mainwindow.cpp
@@ -1,14 +1,59 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    connect(&manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(receiveReply(QNetworkReply*)));
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_requestButton_clicked()
+{
+    QString uri = ui->uriLine->text();
+    QString method = ui->methodCombo->currentText();
+    QString raw_data = ui->dataEdit->toPlainText();
+    QByteArray data;
+    if (ui->dataModeCombo->currentText() == "Hex Mode") {
+        raw_data = raw_data.toUpper().remove(QRegularExpression("[^0-9A-F]"));
+        for (int i = 0; i < raw_data.size(); i += 2)
+            data += raw_data.mid(i, 2).toInt(0, 16);
+    } else {
+        data = raw_data.toUtf8();
+    }
+
+    QString cookies = ui->cookieEdit->toPlainText();
+    if (method == "GET")
+        manager.get(QNetworkRequest(uri));
+    if (method == "POST") {
+        manager.post(QNetworkRequest(uri), data);
+    }
+    if (method == "PUT")
+        manager.put(QNetworkRequest(uri), data);
+    if (method == "DELETE")
+        manager.deleteResource(QNetworkRequest(uri));
+}
+
+void MainWindow::receiveReply(QNetworkReply *reply)
+{
+    QByteArray content = reply->readAll();
+    ui->rawOutput->setPlainText(QString::fromUtf8(content.replace('\0', "\\0")));
+    ui->hexOutput->clear();
+    QString line;
+    for (int i = 0; i < content.size(); i++) {
+        if (!(i & 0xF)) {
+            if (i)
+                ui->hexOutput->append(line);
+            line = QStringLiteral("0x%1").arg(QString::number(i, 16).rightJustified(4, '0', true));
+        }
+        line.append(QStringLiteral(" %1").arg(QString::number(content.at(i), 16).rightJustified(2, '0', true)));
+    }
+    reply->deleteLater();
 }
--- HttpEmulator/mainwindow.h
+++ HttpEmulator/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QRegularExpression>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_requestButton_clicked();
+    void receiveReply(QNetworkReply* reply);
+
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager manager;
 };
 
 #endif // MAINWINDOW_H
