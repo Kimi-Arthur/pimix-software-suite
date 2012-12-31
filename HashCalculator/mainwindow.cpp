4fc6856304d4219aa1e6c8e607229ee340059c67
Kimi <kimi.ribery@gmail.com>
2013-01-01 05:33:39 +0800

    Done for every mode. File mode may be a little slugish and freezing ui.

--- HashCalculator/mainwindow.cpp
+++ HashCalculator/mainwindow.cpp
@@ -6,6 +6,17 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QFormLayout * layout = new QFormLayout();
+    ui->outputGroupBox->setLayout(layout);
+    QString alg[] = {"MD4", "MD5", "SHA-1", "SHA-224", "SHA-256", "SHA-384", "SHA-512"};
+    int algcount = 7;
+    for (int i = 0; i < algcount; i++) {
+        QString name = alg[i];
+        algorithms.append(name);
+        outputLines.append(new QLineEdit());
+        outputLines.back()->setReadOnly(false);
+        layout->addRow(name, outputLines.back());
+    }
 }
 
 MainWindow::~MainWindow()
@@ -30,3 +41,38 @@ void MainWindow::on_fileModeRadio_toggled(bool checked)
     if (checked)
         ui->sourceStackedWidget->setCurrentIndex(2);
 }
+
+void MainWindow::on_hashButton_clicked()
+{
+    if (ui->rawTextModeRadio->isChecked()) {
+        for (int algorithm = 0; algorithm < algorithms.size(); algorithm++) {
+            QCryptographicHash::Algorithm alg = QCryptographicHash::Algorithm(algorithm);
+            QCryptographicHash hasher(alg);
+            hasher.addData(ui->rawTextEdit->document()->toPlainText().toUtf8());
+            outputLines[algorithm]->setText(hasher.result().toHex().toUpper());
+        }
+    }
+    if (ui->hexTextModeRadio->isChecked()) {
+        QString text = ui->hexTextEdit->document()->toPlainText().toUpper().remove(QRegularExpression("[^0-9A-F]"));
+        QByteArray hexData;
+        for (int i = 0; i < text.size(); i += 2)
+            hexData += text.mid(i, 2).toInt(0, 16);
+        for (int algorithm = 0; algorithm < algorithms.size(); algorithm++) {
+            QCryptographicHash::Algorithm alg = QCryptographicHash::Algorithm(algorithm);
+            QCryptographicHash hasher(alg);
+            hasher.addData(hexData);
+            outputLines[algorithm]->setText(hasher.result().toHex().toUpper());
+        }
+    }
+    if (ui->fileModeRadio->isChecked()) {
+        QFile file(ui->fileWidget->getPath());
+        file.open(QIODevice::ReadOnly);
+        for (int algorithm = 0; algorithm < algorithms.size(); algorithm++) {
+            file.reset();
+            QCryptographicHash::Algorithm alg = QCryptographicHash::Algorithm(algorithm);
+            QCryptographicHash hasher(alg);
+            outputLines[algorithm]->setText(hasher.result().toHex().toUpper());
+        }
+    }
+}
+
--- HashCalculator/mainwindow.h
+++ HashCalculator/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCryptographicHash>
+#include <QRegularExpression>
+#include <QLineEdit>
+#include <QFormLayout>
 #include "filechooserwidget.h"
 
 namespace Ui {
@@ -23,8 +27,14 @@ private slots:
 
     void on_fileModeRadio_toggled(bool checked);
 
+    void on_hashButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QStringList algorithms;
+    // Better implementation
+    //    QStringList algorithms = {"MD4", "MD5", "SHA-1", "SHA-224", "SHA-256", "SHA-384", "SHA-512"};
+    QList<QLineEdit *> outputLines;
 };
 
 #endif // MAINWINDOW_H
