78ebbe57ba42044d6e6ff0f1915f24e378c07f1c
Kimi <Kimi@localhost>
2013-09-13 20:23:44 +0800

    PJsonValue

---
+++ PtCore/PJsonValue.h
@@ -0,0 +1,32 @@
+#ifndef PJSONVALUE_H
+#define PJSONVALUE_H
+
+#include "ptcore_global.h"
+
+class QJsonValue;
+
+class PJsonValueRef;
+
+class PTCORESHARED_EXPORT PJsonValue : public QJsonValue
+{
+public:
+    PJsonValue(QJsonValue val) : QJsonValue(val) {}
+    PJsonValue operator [](QString key) const;
+    PJsonValueRef operator [](QString key);
+
+    PJsonValue operator [](int index) const;
+    PJsonValueRef operator [](int index);
+};
+
+class PTCORESHARED_EXPORT PJsonValueRef : public QJsonValueRef
+{
+public:
+    PJsonValueRef(QJsonValueRef ref) : QJsonValueRef(ref) {}
+    PJsonValue operator [](QString key) const;
+    PJsonValueRef operator [](QString key);
+
+    PJsonValue operator [](int index) const;
+    PJsonValueRef operator [](int index);
+};
+
+#endif // PJSONVALUE_H
