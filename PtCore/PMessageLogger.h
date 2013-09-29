e569f28d3f9661098415474f29ff71ed8fffda0e
Kimi <Kimi@localhost>
2013-09-29 20:41:50 +0800

    Renaming 1

--- PtCore/PMessageLogger.h
+++ PtCore/PMessageLogger.h
@@ -8,19 +8,24 @@
 #include "ptcore_global.h"
 #include <QPluginLoader>
 
-class PTCORESHARED_EXPORT PLogger
+namespace Pt {
+namespace Core {
+
+class PTCORESHARED_EXPORT PMessageLogger
 {
 public:
     // Public Types
-    enum LogType {
-        DebugLog, InformationLog, WarningLog, ErrorLog, FatalLog
+    enum class LogType {
+        TraceLog, DebugLog, InformationLog, WarningLog, ErrorLog, FatalLog
     };
 
     // Public Constants
-    const QMap<LogType, QString> LogStrings = {{DebugLog, "DEBUG"}, {InformationLog, "INFO"}, {WarningLog, "WARN"}, {ErrorLog, "ERROR"}, {FatalLog, "FATAL"}};
+    const QMap<LogType, QString> LogStrings = {{LogType::TraceLog, "TRACE"}, {LogType::DebugLog, "DEBUG"},
+                                               {LogType::InformationLog, "INFO"}, {LogType::WarningLog, "WARN"},
+                                               {LogType::ErrorLog, "ERROR"}, {LogType::FatalLog, "FATAL"}};
     const QString DefaultLogPattern = "[{time}][{type}]{content}";
-    const QString DefaultLogFileNamePattern = "{base_path}/{iid}-{date}";
-    const LogType DefaultDisplayBound = InformationLog;
+    const QString DefaultLogFileNamePattern = "{base_path}/{date}/{iid}-{pid}";
+    const LogType DefaultDisplayBound = LogType::InformationLog;
 
     // Public Data Memebers
     QString logPattern; // What the logs contain.
@@ -29,11 +34,11 @@ public:
     QMap<QString, QString> generalParamters; // Customized fill-in parameters.
 
     // Public Constructors
-    PLogger();
-    PLogger(QPluginLoader &loader);
+    PMessageLogger();
+    PMessageLogger(QPluginLoader &loader);
 
     // Public Methods
-    void log(QString content, LogType logType) const;
+    void log(QString content, LogType logType = LogType::TraceLog) const;
     void debug(QString content) const;
     void info(QString content) const;
     void warn(QString content) const;
@@ -44,5 +49,8 @@ private:
     void displayLog(QString resultLog, LogType logType) const;
     void writeLog(QString resultLog, LogType logType) const;
 };
+
+}
+}
 
 #endif // PLOGGER_H
