f35a011c342ae5fcb1bf913a3ea34e1f9798c7d6
Kimi <Kimi@localhost>
2013-10-24 21:32:29 +0800

    Test code

--- BaiduCloudConsole/main.cpp
+++ BaiduCloudConsole/main.cpp
@@ -5,11 +5,14 @@
 
 #include <QDebug>
 
+using namespace Pt::Core;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug() << Pt::Core::PSerializer::serialize(qint64(16));
-    qDebug() << Pt::Core::PSerializer::serialize(QString("123"));
+    qDebug() << PSerializer::serialize(qint64(16));
+    qDebug() << PSerializer::serialize<char *>("123");
+    PLogger::globalInstance()->error("是厉害啊！");
+//    qDebug() << PSerializer::deserialize<char *>("abc");
 //    Pt::Core::PLogger::globalInstance()->logPattern = "abc";
 //    auto parameters = QCoreApplication::arguments();
 //    BaiduCloudWorker w;
