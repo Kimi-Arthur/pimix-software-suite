bba55190c115a2c82ff7df48e7e663c5b34643bc
Kimi <Kimi@localhost>
2013-10-28 21:18:10 +0800

    Added checksum for use for crc

---
+++ PtCore/PChecksum.h
@@ -0,0 +1,34 @@
+#ifndef PCHECKSUM_H
+#define PCHECKSUM_H
+
+#include <QByteArray>
+#include <functional>
+#include "ptcore_global.h"
+
+class PChecksumMethod;
+
+template <class Key, class T>
+class QMap;
+
+class PTCORESHARED_EXPORT PChecksum
+{
+public:
+    enum class Algorithm {
+        Crc16,
+        Crc32,
+        Crc64
+    };
+
+    PChecksum(Algorithm method);
+    ~PChecksum();
+    void reset();
+    void addData(const QByteArray &data);
+    QByteArray result() const;
+
+private:
+    static QMap<Algorithm, std::function<PChecksumMethod *()>> checksumMethods;
+    PChecksumMethod *checksumMethod;
+    Algorithm methodType;
+};
+
+#endif // PCHECKSUM_H
