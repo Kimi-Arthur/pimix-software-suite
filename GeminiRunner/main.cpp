fc0ca89c44d0a68a0d8c7eafd273d631d1f1c50c
Kimi <Kimi@localhost>
2013-11-09 10:32:40 +0800

    Added local file getter

--- GeminiRunner/main.cpp
+++ GeminiRunner/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QDir>
 #include <QStringList>
 #include <QThread>
 
@@ -8,13 +9,25 @@ BaiduCloudWorker w;
 
 QStringList getLocalFileList(const QString &baseLocalPath)
 {
-    return QStringList();
+    qDebug() << baseLocalPath;
+    QDir d(baseLocalPath);
+    QStringList result = d.entryList(QDir::Files).replaceInStrings(QRegularExpression("^"), baseLocalPath + "/");
+    foreach (auto de, d.entryList(QDir::Dirs | QDir::NoDotAndDotDot)) {
+        result.append(getLocalFileList(baseLocalPath + "/" + de));
+    }
+    return result;
 }
 
 QStringList getUpdatedFileList(const QString &baseLocalPath)
 {
     QStringList onlineList = w.getFileList();
     QStringList offlineList = getLocalFileList(baseLocalPath);
+
+    onlineList.replaceInStrings(QRegularExpression("^Backup/"), "");
+    offlineList.replaceInStrings(QRegularExpression("^" + baseLocalPath + "/"), "");
+
+    qDebug() << offlineList;
+
     foreach (auto online, onlineList) {
         offlineList.removeAll(online);
     }
@@ -31,8 +44,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-//    QString basePath = argv[1];
-    QString basePath = "U:/";
+    QString basePath = argv[1];
+//    QString basePath = "C:/Downloads/test";
     while (true) {
         auto fl = getUpdatedFileList(basePath);
         foreach (auto f, fl) {
